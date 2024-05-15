6b723165b69c5083fe2cd489857107ea9cbb7769
Davide Andreolli <dvd2003333@gmail.com>
2024-05-15 18:27:44 +0200

    feat: added main

---
+++ core/main.cpp
@@ -0,0 +1,19 @@
+#include <cstring>
+#include <iostream>
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+
+#include "requests/requests.h"
+#include "requests/device.h"
+
+int main() {
+
+  std::string url = "https://idp.eagletrt.it/realms/master/protocol/openid-connect/auth/device";
+  std::string client_id = "c3363912f2d069ed92e4b4b63612f4da";
+  std::vector<std::string> scopes = {"openid", "email", "profile", "offline_access"};
+ 
+  auto response = device_flow_init(url, client_id, scopes);
+
+  std::cout << response.body["user_code"] << std::endl;
+}
+
