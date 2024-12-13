944b757f2cc68fbab683368d5960576def8be880
Davide Andreolli <dvd2003333@gmail.com>
2024-12-13 16:16:10 +0100

    feat: added main server test

--- core/main.cpp
+++ core/main.cpp
@@ -1,71 +1,72 @@
-#include <string>
-#include <iostream>
 #include <curl/curl.h>
-#include <nlohmann/json.hpp>
 #include <unistd.h>
 
-#include "requests/requests.h"
-#include "requests/openid_configuration.h"
+#include <cstdlib>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <string>
+
 #include "requests/device.h"
+#include "requests/openid_configuration.h"
+#include "requests/requests.h"
 #include "requests/token.h"
+#include "server.h"
 
+/*
 int main() {
-
-  std::string discovery_url = "https://idp.eagletrt.it/realms/master/.well-known/openid-configuration";
+  std::string discovery_url =
+      "https://idp.eagletrt.it/realms/master/.well-known/openid-configuration";
   std::string client_id = "c3363912f2d069ed92e4b4b63612f4da";
-  std::vector<std::string> scopes = {"openid", "email", "profile", "offline_access"};
+  std::vector<std::string> scopes = {"openid", "email", "profile",
+                                     "offline_access"};
 
-  auto discovery_response = request_discover_openid_configuration(discovery_url);
+  auto discovery_response =
+      request_discover_openid_configuration(discovery_url);
   auto device_response = request_device_flow_init(
-    std::string(discovery_response.body["device_authorization_endpoint"]),
-    client_id,
-    scopes
-  );
+      std::string(discovery_response.body["device_authorization_endpoint"]),
+      client_id, scopes);
 
-  std::cout << std::string(device_response.body["verification_uri_complete"]) << std::endl;
+  std::cout << std::string(device_response.body["verification_uri_complete"])
+            << std::endl;
+  system(
+      ("open " + std::string(device_response.body["verification_uri_complete"]))
+          .c_str());
 
   // get int from json with key "interval"
   int interval = device_response.body["interval"];
 
-  device_flow_params_t device_params {
-    std::string(device_response.body["device_code"])
-  };
+  device_flow_params_t device_params{
+      std::string(device_response.body["device_code"])};
 
   response_t token_response;
-  
   do {
     std::cout << "Waiting for user to authenticate..." << std::endl;
     sleep(interval);
-    token_response = request_token(
-      std::string(discovery_response.body["token_endpoint"]),
-      client_id,
-      device_params
-    );
+    token_response =
+        request_token(std::string(discovery_response.body["token_endpoint"]),
+                      client_id, device_params);
   } while (token_response.code != 200);
 
   std::cout << "User authenticated!" << std::endl;
 
   response_t refresh_response;
   size_t token_expiration = token_response.body["expires_in"];
-  
+
   do {
     sleep(token_expiration - 10);
 
-    refresh_flow_params_t refresh_params {
-      std::string(token_response.body["refresh_token"])
-    };
+    refresh_flow_params_t refresh_params{
+        std::string(token_response.body["refresh_token"])};
 
-    refresh_response = request_token(
-      std::string(discovery_response.body["token_endpoint"]),
-      client_id,
-      refresh_params
-    );
+    refresh_response =
+        request_token(std::string(discovery_response.body["token_endpoint"]),
+                      client_id, refresh_params);
 
     if (refresh_response.code == 200) {
       std::cout << "Token refreshed!" << std::endl;
       token_response = refresh_response;
       token_expiration = token_response.body["expires_in"];
-    } 
+    }
 
   } while (refresh_response.code == 200);
 
@@ -73,4 +74,28 @@ int main() {
 
   std::cout << "Token expired!" << std::endl;
 }
+*/
 
+int main() {
+  CallbackResponse params;
+  std::shared_ptr<CallbackResponse> ptr =
+      std::make_shared<CallbackResponse>(params);
+
+  AuthCallbackServer srv;
+  srv.start();
+
+  system(
+      "open \"https://accounts.google.com/o/oauth2/v2/"
+      "auth?client_id=63544120325-1qlcbqe6riugunpn0p29dfnulq6esera."
+      "apps.googleusercontent.com&redirect_uri=http://localhost:8080/"
+      "callback&scope=openid%20profile%20email&response_type=code&"
+      "state=0bbe370cdb9e9dd8852660c05efdfc112a8c2cda\"");
+
+  std::cout << "Server started" << std::endl;
+
+  CallbackResponse response = srv.wait_response(10);
+
+  srv.stop();
+
+  std::cout << "Server stopped" << std::endl;
+}
